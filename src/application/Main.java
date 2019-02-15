e7b516ba649958f50a58ad2fa4890915e528c585
Andrew Galvin <andrewggalvin@gmail.com>
2019-02-14 23:49:09 -0500

    Created board and user interface.

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,19 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gameBoard.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			//test
+			
+			Stage primaryStage3 = new Stage();
+			Parent playerSelect = FXMLLoader.load(getClass().getClassLoader().getResource("playerSelectConnect4.fxml"));
+			Scene scene3 = new Scene(playerSelect);
+			scene3.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage3.setScene(scene3);
+			primaryStage3.show();
+			
+			/*Stage primaryStage2 = new Stage();
+			Parent startMenu = FXMLLoader.load(getClass().getClassLoader().getResource("startMenu.fxml"));
+			Scene scene2 = new Scene(startMenu);
+			scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage2.setScene(scene2);
+			primaryStage2.show();*/
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
